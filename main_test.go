7a0c8a8002270fe59c410586681d90a77d0b54c1
Carlos Nieto <jose.carlos@menteslibres.net>
2013-11-10 09:41:14 -0600

    Drafting more tests.

--- main_test.go
+++ main_test.go
@@ -1,8 +1,123 @@
 package gonekta
 
 import (
+	"fmt"
 	"testing"
 )
+
+const (
+	testKey = `1tv5yJp3xnVZ7eK67m4h`
+)
+
+func TestCreditCard(t *testing.T) {
+
+	payment := &CardPayment{
+		Amount:      20000,
+		Currency:    "mxn",
+		ReferenceId: "000-stoogies",
+		Description: "Stoogies",
+		Card: &Card{
+			Number:   "4111111111111111",
+			ExpMonth: 12,
+			ExpYear:  2015,
+			Name:     "Thomas Logan",
+			CVC:      666,
+			Address: &Address{
+				Street1: "250 Alexis St",
+				City:    "Red Deer",
+				State:   "Alberta",
+				Country: "Canada",
+				Zip:     "T4N 0B8",
+			},
+		},
+	}
+
+	client := New(testKey)
+
+	res, err := client.Pay(payment)
+
+	fmt.Printf("GOT %v\n", res.Payment)
+	fmt.Printf("GOT %v\n", res.Error)
+
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+}
+
+func TestAdvancedCreditCard(t *testing.T) {
+
+	payment := &CardPayment{
+		Amount:      20000,
+		Currency:    "mxn",
+		ReferenceId: "000-stoogies",
+		Description: "Stoogies",
+		Card: &Card{
+			Number:   "4111111111111111",
+			ExpMonth: 12,
+			ExpYear:  2015,
+			Name:     "Thomas Logan",
+			CVC:      666,
+			Address: &Address{
+				Street1: "250 Alexis St",
+				City:    "Red Deer",
+				State:   "Alberta",
+				Country: "Canada",
+				Zip:     "T4N 0B8",
+			},
+		},
+		Details: &Details{
+			Name:        "Wolverine",
+			Email:       "logan@x-men.org",
+			Phone:       "403-342-0642",
+			DateOfBirth: "1980-09-24",
+			BillingAddress: &Address{
+				TaxId:       "xmn671212drx",
+				CompanyName: "X-Men Inc.",
+				Street1:     "77 Mystery Lane",
+				Street2:     "Suite 124",
+				City:        "Darlington",
+				State:       "NJ",
+				Zip:         "10192",
+				Phone:       "77-777-7777",
+				Email:       "purshasing@x-men.org",
+			},
+			LineItems: []LineItem{
+				LineItem{
+					Name:        "Box of Cohiba S1s",
+					SKU:         "cohb_s1",
+					Price:       20000,
+					Description: "Imported from Mex.",
+					Quantity:    1,
+					Type:        "other_human_consumption",
+				},
+			},
+			Shipment: &Shipment{
+				Carrier:    "estafeta",
+				Service:    "international",
+				TrackingId: "XXYYZZ-9990000",
+				Price:      20000,
+				Address: &Address{
+					Street1: "250 Alexis St",
+					City:    "Red Deer",
+					State:   "Alberta",
+					Country: "Canada",
+					Zip:     "T4N 0B8",
+				},
+			},
+		},
+	}
+
+	client := New(testKey)
+
+	res, err := client.Pay(payment)
+
+	fmt.Printf("GOT %v\n", res.Payment)
+	fmt.Printf("GOT %v\n", res.Error)
+
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+}
 
 func TestCash(t *testing.T) {
 
@@ -18,7 +133,80 @@ func TestCash(t *testing.T) {
 		},
 	}
 
-	err := payment.Process()
+	client := New(testKey)
+
+	res, err := client.Pay(payment)
+
+	fmt.Printf("GOT %v\n", res.Payment)
+	fmt.Printf("GOT %v\n", res.Error)
+
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+}
+
+func TestBank(t *testing.T) {
+
+	payment := &BankPayment{
+		Amount:      20000,
+		Currency:    "mxn",
+		Description: "DVD - Zorro",
+		Details: &Details{
+			Name:  "Wolverine",
+			Email: "foo@bar.com",
+			Phone: "403-342-0642",
+		},
+		Bank: &Bank{
+			Name: "banorte",
+		},
+	}
+
+	client := New(testKey)
+
+	res, err := client.Pay(payment)
+
+	fmt.Printf("GOT %v\n", res.Payment)
+	fmt.Printf("GOT %v\n", res.Error)
+
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+}
+
+func TestGet(t *testing.T) {
+	client := New(testKey)
+
+	res, err := client.Get("527fa64f8ee31e4db6000708")
+
+	if res != nil {
+		fmt.Printf("GOT %v\n", res.Payment)
+		fmt.Printf("GOT %v\n", res.Error)
+	}
+
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+}
+
+func TestRefund(t *testing.T) {
+	client := New(testKey)
+
+	res, err := client.Refund("527fa64f8ee31e4db6000708")
+
+	if res != nil {
+		fmt.Printf("GOT %v\n", res.Payment)
+		fmt.Printf("GOT %v\n", res.Error)
+	}
+
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+}
+
+func TestAll(t *testing.T) {
+	client := New(testKey)
+
+	_, err := client.Find()
 
 	if err != nil {
 		t.Fatalf(err.Error())
--- types.go
+++ types.go
@@ -1,32 +1,65 @@
 package gonekta
 
 import (
+	"menteslibres.net/gosexy/rest"
+	"menteslibres.net/gosexy/to"
 	"time"
 )
 
+type Conekta struct {
+	client *rest.Client
+	key    string
+}
+
 type Address struct {
-	Street1 string `json:"street1"`
-	Street2 string `json:"street2"`
-	Street3 string `json:"street3"`
-	City    string `json:"city"`
-	State   string `json:"state"`
-	Country string `json:"country"`
-	Zip     string `json:"zip"`
+	TaxId       string `json:"tax_id"`
+	CompanyName string `json:"company_name"`
+	Phone       string `json:"phone"`
+	Email       string `json:"email"`
+	Street1     string `json:"street1"`
+	Street2     string `json:"street2"`
+	Street3     string `json:"street3"`
+	City        string `json:"city"`
+	State       string `json:"state"`
+	Country     string `json:"country"`
+	Zip         string `json:"zip"`
+}
+
+type Shipment struct {
+	Carrier    string   `json:"carrier"`
+	Service    string   `json:"service"`
+	TrackingId string   `json:"tracking_id"`
+	Price      uint     `json:"price"`
+	Address    *Address `json:"address"`
 }
 
 type Card struct {
-	Number   string `json:"number"`
-	ExpMonth int    `json:"exp_month"`
-	ExpYear  int    `json:"exp_year"`
-	Name     string `json:"name"`
-	CVV      int    `json:"cvv"`
-	Address
+	Number   string   `json:"number"`
+	ExpMonth int      `json:"exp_month"`
+	ExpYear  int      `json:"exp_year"`
+	Name     string   `json:"name"`
+	CVC      int      `json:"cvc"`
+	Address  *Address `json:"address"`
+}
+
+type LineItem struct {
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	SKU         string `json:"sku"`
+	Price       uint   `json:"price"`
+	Quantity    uint   `json:"quantity"`
+	UnitPrice   uint   `json:"unit_price"`
+	Type        string `json:"type"`
 }
 
 type Details struct {
-	Name  string `json:"name"`
-	Phone string `json:"phone"`
-	Email string `json:"email"`
+	Name           string     `json:"name"`
+	Phone          string     `json:"phone"`
+	Email          string     `json:"email"`
+	DateOfBirth    string     `json:"date_of_birth"`
+	LineItems      []LineItem `json:"line_items"`
+	BillingAddress *Address   `json:"billing_address"`
+	Shipment       *Shipment  `json:"shipment"`
 }
 
 type Cash struct {
@@ -37,17 +70,20 @@ type Bank struct {
 	Name string `json:"name"`
 }
 
+type Time struct {
+	time.Time
+}
+
 type Payment struct {
 	Id             string                 `json:"id"`
-	Object         string                 `json:"object"`
 	Livemode       bool                   `json:"livemode"`
-	CreatedAt      time.Time              `json:"created_at"`
+	CreatedAt      Time                   `json:"created_at"`
 	Status         string                 `json:"status"`
 	Description    string                 `json:"description"`
 	Amount         uint                   `json:"amount"`
 	Currency       string                 `json:"currency"`
 	PaymentMethod  map[string]interface{} `json:"payment_method"`
-	Details        Details                `json:"details"`
+	Details        *Details               `json:"details"`
 	ReferenceId    string                 `json:"reference_id"`
 	FailureCode    string                 `json:"failure_code"`
 	FailureMessage string                 `json:"failure_message"`
@@ -62,8 +98,12 @@ type basePayment struct {
 }
 
 type CardPayment struct {
-	basePayment
-	Card
+	Description string   `json:"description"`
+	Amount      uint     `json:"amount"`
+	Currency    string   `json:"currency"`
+	ReferenceId string   `json:"reference_id"`
+	Details     *Details `json:"details"`
+	Card        *Card    `json:"card"`
 }
 
 type CashPayment struct {
@@ -75,6 +115,15 @@ type CashPayment struct {
 	Cash        *Cash    `json:"cash"`
 }
 
+type BankPayment struct {
+	Description string   `json:"description"`
+	Amount      uint     `json:"amount"`
+	Currency    string   `json:"currency"`
+	ReferenceId string   `json:"reference_id"`
+	Details     *Details `json:"details"`
+	Bank        *Bank    `json:"bank"`
+}
+
 type RefundRequest struct {
 	Id     string `json:"id"`
 	Amount uint   `json:"amount"`
@@ -82,11 +131,15 @@ type RefundRequest struct {
 
 type Event struct {
 	Id        string      `json:"id"`
-	Object    string      `json:"object"`
 	Livemode  bool        `json:"livemode"`
-	CreatedAt time.Time   `json:"created_at"`
+	CreatedAt Time        `json:"created_at"`
 	Type      string      `json:"type"`
 	Data      interface{} `json:"data"`
+}
+
+type PaymentResponse struct {
+	*Payment
+	*Error
 }
 
 type Error struct {
@@ -94,4 +147,15 @@ type Error struct {
 	Message string `json:"message"`
 	Code    string `json:"code"`
 	Param   string `json:"param"`
+}
+
+func (self *Time) MarshalJSON() ([]byte, error) {
+	t := self.Unix()
+	return to.Bytes(t), nil
+}
+
+func (self *Time) UnmarshalJSON(b []byte) error {
+	u := to.Int64(b)
+	self.Time = time.Unix(u, 0)
+	return nil
 }
